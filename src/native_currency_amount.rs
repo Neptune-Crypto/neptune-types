79205a3b4a56144e557aafbee61052ba08d64985
danda <dan-da@users.noreply.github.com>
2025-07-02 19:04:28 -0700

    unconditionally serialize i128 as string. neptune-core must do the same

--- src/native_currency_amount.rs
+++ src/native_currency_amount.rs
@@ -49,25 +49,9 @@ use crate::native_currency::NativeCurrency;
 /// inflation bugs.
 #[derive(Clone, Debug, Copy, Serialize, Deserialize, Eq, Default, BFieldCodec)]
 pub struct NativeCurrencyAmount(
-    i128
+    // so i128 can be serialized by serde-json-wasm crate.
+    #[serde(with = "crate::serde::i128")] i128,
 );
-
-#[derive(Clone, Debug, Copy, Serialize, Deserialize, PartialEq, Eq, Default, BFieldCodec)]
-pub struct NativeCurrencyAmountWasm(
-    #[serde(with = "crate::serde::i128")]
-    i128
-);
-impl From<NativeCurrencyAmount> for NativeCurrencyAmountWasm {
-    fn from(value: NativeCurrencyAmount) -> Self {
-        Self(value.0)
-    }
-}
-impl From<NativeCurrencyAmountWasm> for NativeCurrencyAmount {
-    fn from(value: NativeCurrencyAmountWasm) -> Self {
-        Self(value.0)
-    }
-}
-
 
 // impl TasmObject for NativeCurrencyAmount {
 //     fn label_friendly_name() -> String {
